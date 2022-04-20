39fed6f02688e1f3503f96cee71311d14da006ed
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-20 14:23:45 +0200

    Inizio progetto

---
+++ Palindroma/script.js
@@ -0,0 +1,7 @@
+// Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+
+const word = prompt("Inserisci una parola")
+
+function name(params) {
+  
+}
