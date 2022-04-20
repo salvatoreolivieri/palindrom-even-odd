a37fc3b441085485a3feaf8f06c7a40135776a56
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-20 14:48:11 +0200

    Prima bozza check palindromo funzionante aggiunto

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -8,7 +8,6 @@
 */
 
 
-
 // 1. Ottieni le stringhe o i numeri dall'utente
 
 const word = prompt("Inserisci una parola")
@@ -30,6 +29,16 @@ function reverseString (string) {
 console.log(reverseString(word));
 
 
-// function checkPalindromo () {
-  
-// }
+// 3. Funzione check palindromo che confronta la stringa presa dall'utente con quella rovesciata
+
+function checkPalindromo (string) {
+
+  if (reverseString(word) === word) {
+    return console.log("è palindromo");
+  } else {
+    return console.log("Non è palindromo");
+  }
+
+}
+
+console.log(checkPalindromo());
